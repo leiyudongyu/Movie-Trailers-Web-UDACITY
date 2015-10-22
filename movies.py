2da1c0c773924da18c9e01d3903b2fd74a9d4226
雷禹 <leiyu@leiyudeMacBook-Air.local>
2015-10-21 21:06:26 -0500

    submit new project

---
+++ movies.py
@@ -0,0 +1,12 @@
+import webbrowser
+
+class Movie():
+    '''This class defines the format of movie'''
+    def __init__(self, movie_dict):
+        self.title = movie_dict["title"]
+        self.storyline = movie_dict["storyline"]
+        self.trailer_youtube_url = movie_dict["trailer_youtube_url"]
+        self.poster_image_url = movie_dict["poster"]
+
+    def show_trailer(self):
+        webbrowser.open(trailer_youtube_url)
